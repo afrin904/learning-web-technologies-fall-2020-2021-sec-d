475a6f4a181c3212b8534172483079897a5f363a
afrin904 <afrins1598@gmail.com>
2020-11-04 22:04:53 +0600

    bg done

---
+++ LAB_TASK_3.2_PHP/blood_group.php
@@ -0,0 +1,38 @@
+<?php
+    if(isset($_REQUEST['submit'])){
+    	if(!empty($_REQUEST['blood_group']))
+    	{
+	   echo $_REQUEST['blood_group'];
+	}
+	   else {
+	   	echo "Please Select Blood Group";
+	   }
+       }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Blood Group Page</title>
+</head>
+<body>
+
+	<form method="post">
+			<fieldset>
+			<legend><b>BLOOD GROUP</b></legend>
+			<select name="blood_group" >
+				<option selected disabled></option>
+				<option value="A+">A+</option>
+				<option value="A-">A-</option>
+                <option value="B+">B+</option>
+                <option value="B-">B-</option>
+                <option value="O+">O+</option>
+                <option value="O-">O-</option>
+                <option value="AB+">AB+</option>
+                <option value="AB-">AB-</option>
+			</select> <br>
+			<hr>
+            <input type="submit" name="submit" value="Submit"> <br>
+        </fieldset>
+	</form>
+</body>
+</html>
