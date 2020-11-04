acad86758b24aa1800d9db25d0bae70d03ee6c20
afrin904 <afrins1598@gmail.com>
2020-11-04 21:57:10 +0600

    degree done

---
+++ LAB_TASK_3.2_PHP/degree.php
@@ -0,0 +1,35 @@
+<?php
+if(isset($_REQUEST['submit'])){
+	if (!empty($_POST["dept"]))
+	{
+		foreach ($_POST["dept"] as $key => $value) {
+		echo $value."<br>";
+		}
+	}
+	else{
+		echo "At least one of them has to be selected ";
+	}
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Degree Page</title>
+</head>
+<body>
+
+	<form method="post">
+		<fieldset>
+			<legend><b>DEGREE</b></legend>
+			<input type="checkbox" name="dept[]" value="SSC"><b> SSC</b>
+			<input type="checkbox" name="dept[]" value="HSC"><b> HSC</b>
+			<input type="checkbox" name="dept[]" value="BSc"><b> BSc</b>
+			<input type="checkbox" name="dept[]" value="MSc"><b> MSc</b><br>
+			<hr>
+			<input type="submit" name="submit" value="Submit"> <br>
+			</fieldset>
+			
+	</form>
+</body>
+</html>
+
