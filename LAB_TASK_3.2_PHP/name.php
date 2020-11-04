f8b95adea0ff442603e47c9113b94e86fab76696
afrin904 <afrins1598@gmail.com>
2020-11-04 20:41:25 +0600

    name done

--- LAB_TASK_3.2_PHP/name.php
+++ LAB_TASK_3.2_PHP/name.php
@@ -1,16 +1,15 @@
 <?php
-	
-	$data = "";
-
 	if(isset($_REQUEST['submit'])){
 		$name = $_REQUEST['name'];
 		
-		if($name == ""){
-			echo " field required...";
-		}else{
-			$data = $name;
-			echo $data;
+		if(empty($name)){
+			echo "Please Enter Your Name";
+		}elseif (!preg_match("/^[a-zA-Z'-''.'\s]+$/",$name)) {
+			echo "Please Enter Name as String and start with a letter ";
+		}elseif (!preg_match("/^.{2}$/",$name)) {
+			echo "Please Enter at least two words";
 		}
+		else{ echo $_POST['name'];}
 	}
 ?>
 
@@ -20,10 +19,10 @@
 	<title>Name</title>
 </head>
 <body>
-	<form method="post" >
+	<form method="post">
 		<fieldset>
 			<legend><b>NAME</b></legend>
-			<input type="text" name="name" value=" <?=$data ?>"><br>
+			<input type="text" name="name" value=""><br>
 			<hr>
 			<input type="submit" name="submit" value="Submit">
 		</fieldset>
