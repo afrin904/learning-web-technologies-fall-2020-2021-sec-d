4df1145928214049df12dc2cc32858f9de2769ab
afrin904 <afrins1598@gmail.com>
2020-11-05 22:03:20 +0600

    registration

--- LAB_TASK_4.1_SESSION/registration.php
+++ LAB_TASK_4.1_SESSION/registration.php
@@ -9,7 +9,7 @@
 			<td width="100px">
 				<img src="pic1.png" width="100px" height="100px" ></td>
 				<td align="right">
-				<a href="home_page.php"><b>HOME</b></a> | 
+				<a href="public_home.php"><b>Home</b></a> | 
 				<a href="login.php"><b>Login</b></a> | 
 				<a href="registration.php"><b>Registration</b></a> 
 			</td>
@@ -52,7 +52,7 @@
 	</tr>	
 		<tr >
 			<td colspan="3" align="center">
-				copyright@2020
+				Copyright@2017
 			</td>
 		</tr>
 	</table>
