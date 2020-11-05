830b73b5a4aa2bf1224661eccacfe84104bfe6c8
afrin904 <afrins1598@gmail.com>
2020-11-05 22:04:04 +0600

    login

--- LAB_TASK_4.1_SESSION/login.php
+++ LAB_TASK_4.1_SESSION/login.php
@@ -1,6 +1,23 @@
+<?php
+
+	if(isset($_REQUEST['msg'])){
+		if($_REQUEST['msg'] == 'null'){
+			echo "null submission...";
+		}
+
+		if($_REQUEST['msg'] == 'invalid'){
+			echo "invalid username/password";
+		}
+
+		if($_REQUEST['msg'] == 'login_first'){
+			echo "please login first...";
+		}
+	}
+?>
+<!DOCTYPE html>
 <html>
 <head>
-	<title>PUBLIC HOME</title>
+	<title>LOGIN</title>
 </head>
 <body>
 
@@ -9,23 +26,23 @@
 			<td width="100px">
 				<img src="pic1.png" width="100px" height="100px" ></td>
 				<td align="right">
-				<a href="home_page.php"><b>Home</b></a> | 
+				<a href="public_home.php"><b>Home</b></a> | 
 				<a href="login.php"><b>Login</b></a> | 
 				<a href="registration.php"><b>Registration</b></a> 
 			</td>
 		</tr>
 		<tr height="200px">
 			<td colspan="3">
-				<form>
+				<form method="POST" action="login_dash.php">
 					<fieldset>
 						<legend>LOGIN</legend>
 
-						User Name:<input type="text" name="uname" value=""><br>
+			User Name:<input type="text" name="uname" value=""><br>
 	    	  <hr>
 	    	  Password:<input type="Password" name="password" value=""><br>
 	    	  <hr>
 	    	  <input type="checkbox" name="rm" value="">Remember Me<br>
-	    	  <input type="submit" name="submit" value="Submit">
+	    	  <input type="submit" name="submit" value="submit">
 	    	  <a href="forget_password.php"><b>Forget Password?</b></a> 
 
 					</fieldset>
@@ -35,7 +52,7 @@
 				
 		<tr height="50px">
 			<td colspan="3" align="center">
-				copyright@2020
+				Copyright@2017
 			</td>
 		</tr>
 	</table>
