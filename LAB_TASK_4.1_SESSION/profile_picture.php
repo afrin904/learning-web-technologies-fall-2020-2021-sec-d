643b557d78d442529396e99adcb054878eb1c8cf
afrin904 <afrins1598@gmail.com>
2020-11-06 00:14:22 +0600

    profile pic

---
+++ LAB_TASK_4.1_SESSION/profile_picture.php
@@ -0,0 +1,60 @@
+<?php
+	session_start();
+	if(isset($_SESSION['flag'])){
+?>
+<html>
+<head>
+	<title>VIEW PROFILE</title>
+</head>
+<body>
+
+	<table width="900px" border="1" align="center">
+		<tr colspan="3">
+			<td width="100px">
+				<img src="pic1.png" width="100px" height="100px" ></td>
+				<td align="right">
+				<b>Logged in as <a href="login_dash.php"><?php echo $_SESSION['uname']?></a>|
+	             <a href="logout.php">Logout</a></b>
+			</td>
+		</tr>
+		<tr width="200px">
+			<td width="200px">
+				<b>Account</b>
+				<hr>
+				<ul>
+				<li><a href="login_dash.php">Dashboard</a></li>
+				<li><a href="view_profile.php">View Profile</a></li>
+				<li><a href="edit_profile.php">Edit Profile</a></li>
+				<li><a href="profile_picture.php">Change Profile Picture</a></li>
+				<li><a href="change_password.php">Change Password</a></li>
+				<li><a href="logout.php">Logout</a></li>
+				</ul>
+			</td>
+			<td>
+				<form>
+					<fieldset>
+						<legend><b>PROFILE PICTURE</b></legend>
+						<img src="pic2.png" width="100px" height="100px" ><br>
+                         <a href="profile_picture.php">
+                         <input type="file" name="picture"  > <hr>
+						<input type="submit" name="submit" value="Submit">
+					</fieldset>
+				</form>
+			</td>
+		</tr>
+				
+		<tr height="50px">
+			<td colspan="3" align="center">
+				Copyright@2017
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
+<?php
+
+	}else{
+		header('location: login.php?msg=login_first');
+	}
+
+?>
