0f25d322219f1c81536b6ab4d7ae92254108bcb7
afrin904 <afrins1598@gmail.com>
2020-11-05 23:27:43 +0600

    login dash

--- LAB_TASK_4.1_SESSION/login_dash.php
+++ LAB_TASK_4.1_SESSION/login_dash.php
@@ -13,19 +13,19 @@
 			<td width="100px">
 				<img src="pic1.png" width="100px" height="100px" ></td>
 				<td align="right">
-				<h1>Logged in as <?php echo $_SESSION['uname']?></h1>
-	             <a href="logout.php">Logout</a>
+				<b>Logged in as <a href="login_dash.php"><?php echo $_SESSION['uname']?></a>|
+	             <a href="logout.php">Logout</a></b>
 			</td>
 		</tr>
-		<tr height="200px">
-			<td>
+		<tr>
+			<td width="200px">
 				<b>Account</b>
 				<hr>
 				<ul>
-				<li><a href="login_dash">Dashboard</a></li>
+				<li><a href="login_dash.php">Dashboard</a></li>
 				<li><a href="view_profile.php">View Profile</a></li>
 				<li><a href="edit_profile.php">Edit Profile</a></li>
-				<li><a href="change_profile_picture.php">Change Profile Picture</a></li>
+				<li><a href="profile_picture.php">Change Profile Picture</a></li>
 				<li><a href="change_password.php">Change Password</a></li>
 				<li><a href="logout.php">Logout</a></li>
 				</ul>
