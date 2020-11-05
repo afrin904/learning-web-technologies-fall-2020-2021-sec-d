6ba8c3351a10d7a32ca092af986f9e202a2434c3
afrin904 <afrins1598@gmail.com>
2020-11-05 10:11:06 +0600

    public home

---
+++ LAB_TASK_4.1_SESSION/public_home.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+	<title>PUBLIC HOME</title>
+</head>
+<body>
+
+	<table width="900px" border="1" align="center">
+		<tr>
+			<td width="100px"><img src="pic1.png" width="100px" height="100px" colspan="3">
+				<a href="home_page.php"><b>HOME</b></a> | 
+				<a href="login.php"><b>CONTACT</b></a> | 
+				<a href="registration.php"><b>ABOUT</b></a> 
+			</td>
+		</tr>
+		<tr height="200px">
+			<td colspan="3" align="left">
+				Welcome to xCompany
+			</td>
+		</tr>
+				
+		<tr height="50px">
+			<td colspan="3" align="center">
+				copyright@2020
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
