975da0a025c36e339050c6cc8390d69a7ffbd5ab
afrin904 <afrins1598@gmail.com>
2020-11-05 12:09:01 +0600

    Forget password

---
+++ LAB_TASK_4.1_SESSION/forget_password.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+	<title>PUBLIC HOME</title>
+</head>
+<body>
+
+	<table width="900px" border="1" align="center">
+		<tr colspan="3">
+			<td width="100px">
+				<img src="pic1.png" width="100px" height="100px" ></td>
+				<td align="right">
+				<a href="home_page.php"><b>Home</b></a> | 
+				<a href="login.php"><b>Login</b></a> | 
+				<a href="registration.php"><b>Registration</b></a> 
+			</td>
+		</tr>
+		<tr height="200px">
+			<td colspan="3" >
+				<form>
+					<fieldset>
+						<legend>FORGET PASSWORD</legend>
+						 Enter Email:<input type="email" name="email" value="" >
+						 <hr> 
+						 <input type="submit" name="submit" value="Submit">
+					</fieldset>
+				</form>
+			</td>
+		</tr>
+				
+		<tr height="50px">
+			<td colspan="3" align="center">
+				copyright@2020
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
