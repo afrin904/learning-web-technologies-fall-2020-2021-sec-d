593e6bb04bd4580c7b0977cdb42337efc8111dfc
afrin904 <afrins1598@gmail.com>
2020-11-11 09:21:17 +0600

    profile

---
+++ MID_LAB_EXAM/profile.php
@@ -0,0 +1,1 @@
+<center>	<table border="1" cellpadding="5" cellspacing="0">		<tr><td colspan="2" align="CENTER">Profile</td></tr>		<tr><td>ID</td><td>16-10101-2</tr>		<tr><td>NAME</td><td>Bob</td></tr>			<tr><td>USER TYPE</td><td>Admin</td></tr>		<tr><td colspan="2" align="right"><a href="admin_home.php">Go Home</a></td></tr>	</table>			</center>
