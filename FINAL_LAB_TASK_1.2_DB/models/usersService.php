f18d5a359bc677294103daf678f3b063bfdd23ea
afrin904 <afrins1598@gmail.com>
2020-11-30 20:10:00 +0600

    models

---
+++ FINAL_LAB_TASK_1.2_DB/models/usersService.php
@@ -0,0 +1,91 @@
+<?php
+
+	require_once('../models/db.php');
+
+	function validate($user){
+			
+		$conn = getConnection();
+		$sql = "select * from users where username='{$user['username']}' and password='{$user['password']}'";
+		$result = mysqli_query($conn, $sql);
+		$row = mysqli_fetch_assoc($result);
+
+		if(count($row) > 0){
+			return true;
+		}else{
+			return false;
+		}
+
+	}
+
+
+	function getById(){
+
+		$conn = getConnection();
+		$sql = "select * from users";
+		$result = mysqli_query($conn, $sql);
+
+		$user =[];
+
+		while($data = mysqli_fetch_assoc($result)){
+			array_push($user, $data);
+		}
+
+		return $user;
+	}
+
+
+	function getAllUsers(){
+
+		$conn = getConnection();
+		$sql = "select * from users";
+		$result = mysqli_query($conn, $sql);
+
+		$user =[];
+
+		while($data = mysqli_fetch_assoc($result)){
+			array_push($user, $data);
+		}
+
+		return $user;
+	}
+
+
+	function insertUsers($user){
+
+		$conn = getConnection();
+		$sql = "insert into users values('{$user['id']}', '{$user['username']}', '{$user['password']}', '{$user['email']}', '{$user['type']}')";
+
+		$status = mysqli_query($conn, $sql);
+
+		if($status){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+
+	function deleteUsers($id){
+		$conn = getConnection();
+		$sql = "delete from users where id=$id";
+		$status = mysqli_query($conn, $sql);
+		if($status){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	function updateUsers($user){
+		$conn = getConnection();
+		$sql ="update users set '{$user['id']}', '{$user['username']}', '{$user['password']}', '{$user['email']}', '{$user['type']}' WHERE id='$id'";
+	    $status = mysqli_query($conn, $sql);
+	    if($status){
+			return true;
+		}else{
+			return false;
+		}
+
+	}
+
+?>
