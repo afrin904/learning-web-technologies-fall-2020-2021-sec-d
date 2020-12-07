d506f8950c7e3cfcea29266c79a69b8aada40a00
afrin904 <afrins1598@gmail.com>
2020-12-07 09:51:24 +0600

    name

---
+++ FINAL_LAB_TASK_JS_3.1/script.js
@@ -0,0 +1,36 @@
+"use strict"
+
+
+//alert('test');
+//console.log('testing...');
+
+//document.write('this is JS example');
+
+//document.getElementById('head').innerHTML = 'this is js example';
+//obj.innerHTML = "test";
+
+
+//let student = ['alamin', '123', 'CS'];
+
+function f1(){
+	let data[]= document.getElementById('name').value;
+
+	if(data == ''){
+		document.getElementById('head').innerHTML = 'Null Submission ';		
+	}
+	else if(data.length <2){
+		document.getElementById('head').innerHTML = 'Length is too Short';	
+	}
+	
+	else if(data<='a' && data >='z' || data<='A' && data >='Z' || data == '.' || data == '-'){
+		document.getElementById('head').innerHTML = data;
+     }
+     else if(){
+     	document.getElementById('head').innerHTML = 'Must start with letter';
+     }
+    else{
+		document.getElementById('head').innerHTML = data;
+	}
+	
+
+}
